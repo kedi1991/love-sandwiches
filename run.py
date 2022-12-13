5d7a611530d3fbfc140b4ec8af4fa2718644843a
kedi1991 <okurut.henry@yahoo.com>
2022-12-13 14:27:25 +0000

    Adjust valiation to check if input is only integers

--- run.py
+++ run.py
@@ -13,5 +13,26 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("love_sandwiches")
 sales = SHEET.worksheet("sales")
-data = sales.get_all_values()
-print(data)
+# data = sales.get_all_values()
+# print(data)
+
+def get_Sales_data():
+    """get daat from the terminal
+    """
+    print("Enter in the format (x,y,z,a,b,c)")
+    data = input("Please enter the sales: ")
+    split_data = data.split(",")
+    validate_data(split_data)
+
+def validate_data(data):
+    """check the data is 6 elements and integers only"""
+    try:
+        [int(num) for num in data]
+
+        if len(data) != 6:
+            raise ValueError(
+                print(f"We need 6 elements, you provided {len(data)}")
+            )
+    except ValueError as e:
+        print(f"You have entered invalid data: {e}")    
+get_Sales_data()
